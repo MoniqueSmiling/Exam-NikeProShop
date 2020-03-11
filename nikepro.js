535f7eb535cdd6817f77b446b7275951137b32e3
Lola Jahn <lolajahn@gmail.com>
2020-03-11 11:24:48 +0100

    Fandt en fejl i aoShoes, addede mine egne alt beskrivelser til aoShoes

--- nikepro.js
+++ nikepro.js
@@ -7,7 +7,7 @@ const aoShoes = [
         img:'img/Nike-orange.jpg',
         name:'Nike Air Max 720',
         description:`Opfrisk din swoosh-stil med disse herre Air Max 720 trainers.`,
-        alt: '',
+        alt: 'Nike Air Max med orange sål, hælen er en orange farvet gennemsigtig plast bobbel imellem sål og sko. og sort top parti.',
         price: 1550,
 
     },
@@ -15,7 +15,7 @@ const aoShoes = [
         img: 'img/Nike-aqua.jpg' ,
         name: 'Nike Air Max 200',
         description: 'Bølgede linjer er inspireret af strømmen af lava og havets rytmiske bølger.',
-        alt: '',
+        alt: 'Nike Air Max med hvid og aqua blå sål, samt en mindre gennemsigtig plastbobbel på siden af hælen, sort Nike logo på siden, coral rød kant hvide snørebånd samt et sort, aqua og cyan farvet pattern.',
         price: 998,
 
     },
@@ -23,7 +23,7 @@ const aoShoes = [
         img: 'img/Nike-aquamarine.jpg',
         name: 'Nike Air Max 90 HYP Womens',
         description: 'Opfrisk din swoosh-stil med disse kvinde Air Max 90 trainers - special color edition.',
-        alt: '',
+        alt: 'Nike Air Max med cyan farvet sål, cyan farvet gennemsigtig plast bobbel imellem sål og sko. Nike logo på siden. Glidende overgang fra cyan til marineblå.',
         price: 1550,
 
     },
@@ -31,7 +31,7 @@ const aoShoes = [
         img: 'img/Nike-bauhaus.jpg',
         name: 'Nike Air Max 200',
         description: 'Bølgede linjer er inspireret af strømmen af lava og havets rytmiske bølger.',
-        alt: '',
+        alt: 'Nike Air Max med hvid og skyblue sål, samt en mindre gennemsigtig pink plastbobbel på siden af hælen, sort Nike logo på siden, lyse violet kant pink snørebånd samt et okker gult, grønt og hvidt farvet pattern.',
         price: 1200,
 
     },
@@ -39,7 +39,7 @@ const aoShoes = [
         img:'img/Nike-blackwhite.jpg',
         name:'Nike Air Max 200 BW',
         description:'Vi har længe vidst at denne silhuet var på trapperne, og er glade for at den kom.',
-        alt: '',
+        alt: 'Nike Air Max med hvid og sort sål, samt en mindre knapt gennemsigtig sort plastbobbel på siden af hælen, sort Nike logo på siden, grøn og coral rød kant, sorte snørebånd samt et sort og hvidt farvet pattern.',
         price: 1200,
 
     },
@@ -47,15 +47,15 @@ const aoShoes = [
         img:'img/Nike-greenyellow.jpg',
         name:'Nike Air Max Retro Future(GS)',
         description:'High Voltage 720. Fremtid og fortid i perfekt match.',
-        alt: '',
+        alt: 'Nike Air Max med lysegrøn farvet sål, lysegrøn farvet gennemsigtig plast bobbel imellem sål og sko. Glidende overgang fra lysegrøn til sort med et pixeleret pattern. Sorte snørebånd.',
         price: 1899,
 
     },
     {
         img:'img/Nike-pastel.jpg',
-        name:'Nike Air Max 270 Phantom releaser d. 3. juli, så sid klar ved tasterne, hvis du vil have fat i et par.',
-        description:'',
-        alt: '',
+        name:'Nike Air Max 270 Phantom',
+        description:'Nike Air Max 270 Phantom releaser d. 3. juli, så sid klar ved tasterne, hvis du vil have fat i et par.',
+        alt: 'Nike Air Max med hvid forparti og sort bagparti på sål, gult farvet gennemsigtig plast bobbel imellem sål og sko. sort Nike logo på siden, tildels dækket af coral rød, baby blå og hvidt pattern. Bright gul kant og hvide snørebånd.',
         price: 1100,
 
     },
@@ -63,7 +63,7 @@ const aoShoes = [
         img:'img/Nike-whitered.jpg',
         name:'Nike Air Max Older Kids',
         description:'Opfrisk din swoosh-stil med disse herre Air Max 720 trainers.',
-        alt: '',
+        alt: 'Hvid og mørkerøde Nike Air Max sko. Med rød plasthæl. Et lille rødt Nike logo på snude af skoen og et større rødt på siden af skoen. Hvide snørrebunder. Skoen har åndehuller.',
         price: 1200,
 
     },
@@ -71,7 +71,7 @@ const aoShoes = [
         img:'img/Nike-rainbow.jpg',
         name:'Nike Air Max Metallic Pink',
         description:'Opfrisk din swoosh-stile med disse here Air Max 720 trainers.',
-        alt: '',
+        alt: 'Regnbue farvet Air Max sko med hvide hæle, 4 små gennemsigtige air bubbles på siden af hælen. Farverne orange, lyserøde, lilla, blå og hvid fordeler sig smoothly udover et strippet pattern.',
         price: 1200,
 
     },
