d2c22a517fdda5f32a1a0df079834c0383918656
Lola Jahn <lolajahn@gmail.com>
2020-03-11 10:52:23 +0100

    created textfile shoes.txt and rewrote the names, descriptions, price and image path into it. Created an array of objects for each shoe. Kept a template at the bottom of the array for future use.

--- README.md
+++ README.md
@@ -14,7 +14,10 @@ Before  I start I'm making a checklist based on the exam document, making sure I
 * Add SEO
     * Meta Description 
         "Nike Pro Shop har skoen til alle dine behov. Fra de populære mærker til hverdagsbrug til de sjældnere eksemplarer til samleren. Vi har det hele! Vores butik ligger placeret i Skive, men vi sælger og sender til hele Danmark."
-
 * Set up the HTML
-    *  
+    *  Created header, main, header, aside, footer
+* Set up the JS file
+    * Set up array objects injavascript for generating shoes.
+
+
 * Create CSS sheet
---
+++ nikepro.js
@@ -0,0 +1,87 @@
+console.log("I'm here!");
+
+// Array of objects 
+// namegiving - a = array, o = object(s)
+const aoShoes = [
+    {
+        img:'img/Nike-orange.jpg',
+        name:'Nike Air Max 720',
+        description:`Opfrisk din swoosh-stil med disse herre Air Max 720 trainers.`,
+        alt: '',
+        price: 1550,
+
+    },
+    {
+        img: 'img/Nike-aqua.jpg' ,
+        name: 'Nike Air Max 200',
+        description: 'Bølgede linjer er inspireret af strømmen af lava og havets rytmiske bølger.',
+        alt: '',
+        price: 998,
+
+    },
+    {
+        img: 'img/Nike-aquamarine.jpg',
+        name: 'Nike Air Max 90 HYP Womens',
+        description: 'Opfrisk din swoosh-stil med disse kvinde Air Max 90 trainers - special color edition.',
+        alt: '',
+        price: 1550,
+
+    },
+    {
+        img: 'img/Nike-bauhaus.jpg',
+        name: 'Nike Air Max 200',
+        description: 'Bølgede linjer er inspireret af strømmen af lava og havets rytmiske bølger.',
+        alt: '',
+        price: 1200,
+
+    },
+    {
+        img:'img/Nike-blackwhite.jpg',
+        name:'Nike Air Max 200 BW',
+        description:'Vi har længe vidst at denne silhuet var på trapperne, og er glade for at den kom.',
+        alt: '',
+        price: 1200,
+
+    },
+    {
+        img:'img/Nike-greenyellow.jpg',
+        name:'Nike Air Max Retro Future(GS)',
+        description:'High Voltage 720. Fremtid og fortid i perfekt match.',
+        alt: '',
+        price: 1899,
+
+    },
+    {
+        img:'img/Nike-pastel.jpg',
+        name:'Nike Air Max 270 Phantom releaser d. 3. juli, så sid klar ved tasterne, hvis du vil have fat i et par.',
+        description:'',
+        alt: '',
+        price: 1100,
+
+    },
+    {
+        img:'img/Nike-whitered.jpg',
+        name:'Nike Air Max Older Kids',
+        description:'Opfrisk din swoosh-stil med disse herre Air Max 720 trainers.',
+        alt: '',
+        price: 1200,
+
+    },
+    {
+        img:'img/Nike-rainbow.jpg',
+        name:'Nike Air Max Metallic Pink',
+        description:'Opfrisk din swoosh-stile med disse here Air Max 720 trainers.',
+        alt: '',
+        price: 1200,
+
+    },
+    // template for adding more shoes later
+    // {
+    //     img:'',
+    //     name:'',
+    //     description:'',
+    //     alt: '',
+    //     price:,
+
+    // },
+]
